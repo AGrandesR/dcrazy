d68dda48e20ddd175ec0f530c96445114662bd99
A.Grandes.R <agrandes11@outlook.es>
2021-04-28 22:22:00 +0200

    Recreated file, because we lost :(

--- src/Utils/DBtool.js
+++ src/Utils/DBtool.js
@@ -1,14 +1,34 @@
 require('dotenv').config()
+const mysql = require('mysql');
 
 
+async function call(flag, sql, params=[]) {
+  return new Promise((res,rej)=>{
+    if(flag!='') flag+='_'
+    const connection = mysql.createConnection({
+      host     : process.env[`${flag}HOST`],
+      user     : process.env[`${flag}USER`],
+      password : process.env[`${flag}PASS`],
+      database : process.env[`${flag}DB`],
+      port     : process.env[`${flag}PORT`]
+    });
+    
+    connection.connect();
+    
+    connection.query(sql, params, function (error, results, fields) {
+      if (error) rej(error);
+      console.log('---------------')
+      console.log(results)
+      console.log('---------------')
+      console.log(fields)
+      console.log('---------------')
+      res(results)
 
-class DBtool {
-    constructor(name, year) {
-      this.name = name;
-      this.year = year;
-    }
+      connection.end();
+    });
+    
+    
+  })
+}
 
-    static call(flag, sql, params) {
-        //MAKE THE CALL TO THE DATABASE USING THE FLAG
-    }
-  }
+module.exports = call
