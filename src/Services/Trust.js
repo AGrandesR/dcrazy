0a0b90a2b2af0b04b3f77054f98bdfe50d075790
A.Grandes.R <agrandesr@gmail.com>
2021-08-22 15:50:33 +0200

    send trust

--- src/Services/Trust.js
+++ src/Services/Trust.js
@@ -9,7 +9,18 @@ const jResponse = require('./Common/jsonResponse')
 //POST /register?(&lng)
 //{dni:****,mail:****,pass:****}  /TOKEN INFO
 async function sendTrust(req, res) {
+    const id = req.userData.id
+    const trust = req.body.id
+    try {
+        const rawResponse = await call('DC', 'UPDATE citizen SET trust=/**/ WHERE id=/**/',[trust,id])
+    } catch (e) {
+        console.log(e)
+        return jResponse(req,res,601)
+    }
     
+    
+    //if(rawResponse[0].trust == null) return jResponse(req,res,502)
+    return jResponse(req,res,500,{"check in": `https://${req.host}/trust`})
 }
 
 async function readTrust(req, res) {
